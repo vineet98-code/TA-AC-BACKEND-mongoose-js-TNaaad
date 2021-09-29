8eef9f60ef3455daec6e707d6cd66a9397026a97
Vineet <vineet98love@gmail.com>
2021-09-29 12:32:36 +0530

    update

--- block-BNaacd/models/user.js
+++ block-BNaacd/models/user.js
@@ -1,16 +1,17 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var userSchema = new Schema({
-    name : String,
-    age : Number,
-    isAdmin : Boolean,
-    createAt : Date
-}, {timestamps: true})
+// var userSchema = new Schema({
+//     name : String,
+//     email : String,
+//     age : Number,
+//     isAdmin : Boolean,
+//     createAt : Date
+// }, {timestamps: true})
 
 var userSchema = new Schema({
     name : { type: String , lowercase: true },
     email : { type: String , lowercase: true },
-    age : { type: Number, value: 0}
+    age : { type: Number, default: 0}
 })
 
