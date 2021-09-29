5ace81bbb57ca79f6e8dd6f1f5f4230735f57d71
Vineet <vineet98love@gmail.com>
2021-09-29 13:20:19 +0530

    block BNaacj

--- block-BNaacj/models/user.js
+++ block-BNaacj/models/user.js
@@ -5,6 +5,6 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
     name :  {type : String, required : true },
     password :  {type : String, minlength: 5, maxlength: 15 },
-    createAt : { type : Date, default: }
+    createAt : { type : Date, default: new Date()}
 
 }, {timestamps: true})
